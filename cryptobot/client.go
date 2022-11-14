ea56cc94664c5aeffd1442ee919dac2aeb6cc217
arthurshafikov <arthur.shafikov27@gmail.com>
2022-11-14 10:09:37 +0300

    add httpClient & client.request() function

--- cryptobot/client.go
+++ cryptobot/client.go
@@ -1,25 +1,48 @@
 package cryptobot
 
+import (
+	"io"
+	"net/http"
+	"net/url"
+	"time"
+)
+
 const (
-	testingAPIURL = "https://testnet-pay.crypt.bot/api/"
-	realAPIURL    = "https://pay.crypt.bot/api/"
+	testingAPIURL      = "https://testnet-pay.crypt.bot/api/"
+	realAPIURL         = "https://pay.crypt.bot/api/"
+	apiTokenHeaderName = "Crypto-Pay-API-Token"
 )
 
 type Client struct {
 	testingMode bool
 	apiToken    string
+
+	httpClient *http.Client
 }
 
 type Deps struct {
 	Testing  bool
 	ApiToken string
+
+	// Optional. Default is 10 seconds
+	ClientTimeout time.Duration
 }
 
 func NewClient(deps Deps) *Client {
-	return &Client{
+	c := &Client{
 		testingMode: deps.Testing,
 		apiToken:    deps.ApiToken,
 	}
+
+	clientTimeout := time.Second * 10
+	if deps.ClientTimeout != 0 {
+		clientTimeout = deps.ClientTimeout
+	}
+	c.httpClient = &http.Client{
+		Timeout: clientTimeout,
+	}
+
+	return c
 }
 
 func (c *Client) getRequestUrl() string {
@@ -28,4 +51,20 @@ func (c *Client) getRequestUrl() string {
 	} else {
 		return realAPIURL
 	}
+}
+
+func (c *Client) request(path string, queryModifierFunc func(q url.Values) url.Values) (io.ReadCloser, error) {
+	req, err := http.NewRequest(http.MethodGet, c.getRequestUrl()+path, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.URL.RawQuery = queryModifierFunc(req.URL.Query()).Encode()
+
+	req.Header.Set(apiTokenHeaderName, c.apiToken)
+	r, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return r.Body, nil
 }
