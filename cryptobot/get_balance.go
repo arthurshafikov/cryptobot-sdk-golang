244afdd7587a60a0a75c5df8dbec3a05d1a1b01f
arthurshafikov <arthur.shafikov27@gmail.com>
2022-11-14 11:25:57 +0300

    add GetBalance method

---
+++ cryptobot/get_balance.go
@@ -0,0 +1,36 @@
+package cryptobot
+
+import "fmt"
+
+type GetBalanceResponse struct {
+	Response
+	Result Balance `json:"result"`
+}
+
+type Balance []BalanceAsset
+
+// for example cryptobot.BalanceAsset{Available:"0", CurrencyCode:"BTC"}
+type BalanceAsset struct {
+	Available    string `json:"available"`
+	CurrencyCode string `json:"currency_code"`
+}
+
+// Use this method to get a balance of your app. Returns slice of BalanceAssets.
+func (c *Client) GetBalance() (*Balance, error) {
+	responseBodyReader, err := c.request("getBalance", nil)
+	if err != nil {
+		return nil, err
+	}
+	defer responseBodyReader.Close()
+
+	var response GetBalanceResponse
+	if err := c.decodeResponse(responseBodyReader, &response); err != nil {
+		return nil, err
+	}
+
+	if response.Ok {
+		return &response.Result, nil
+	} else {
+		return nil, fmt.Errorf("getBalance request error: code - %v, name - %s", response.Error.Code, response.Error.Name)
+	}
+}
